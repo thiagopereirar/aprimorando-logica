95b10b2974575922a0c0baae37427e0fa9136fa6
thiagopereirar <thpereira.r@gmail.com>
2025-12-11 08:19:36 -0300

    Alterações para deixar o código mais interativo e limpo.

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const prompt = require('prompt-sync')();
 
-const nomes = [ "Batman", "Kratos", "Sakura", "Goku", "Naruto", "Luffy", "Jinx" ];
+const campeoes = ["Batman", "Kratos", "Sakura", "Goku", "Naruto", "Luffy", "Jinx"];
 
 
-function nomeHeroi(){
-for(let i = 0 ; i < nomes.length ; i++ )
-    {
-        let selecao = nomes[i]
+function nomeHeroi() {
+    for (let i = 0; i < campeoes.length; i++) {
+        let selecao = campeoes[i]
         console.log("Seleção " + i + ": " + selecao)
 
-}
+    }
 
 }
 
@@ -19,16 +18,15 @@ nomeHeroi()
 console.log("---------------")
 
 
+let heroiSelecionado
 
-let heroiSelecionado 
-
-while(!heroiSelecionado){
+while (!heroiSelecionado) {
     let escolha = Number(prompt("Escolha seu Campeão pelo número: "));
-     heroiSelecionado = nomes[escolha];
-    
-    
+    heroiSelecionado = campeoes[escolha];
 
-    if (!heroiSelecionado){
+
+
+    if (!heroiSelecionado) {
         console.log("Opção Inválida! Tente de novo.")
     }
 }
@@ -38,33 +36,34 @@ console.log("Você escolheu: " + heroiSelecionado)
 console.log("----- Escolha o XP ----- ")
 console.log("XP de 0 a 10001")
 
-     
+
 let xpHeroi = Number(prompt("Digite o xp do seu Campeão: "))
 let nivel = ""
 if (xpHeroi < 1000) {
     nivel = "Ferro";
 }
-else if(xpHeroi <= 2000){
+else if (xpHeroi <= 2000) {
     nivel = "Bronze";
 }
-else if(xpHeroi <= 5000){
+else if (xpHeroi <= 5000) {
     nivel = "Prata";
 }
-else if (xpHeroi <= 7000){
+else if (xpHeroi <= 7000) {
     nivel = "Ouro";
 }
-else if (xpHeroi <= 8000){
+else if (xpHeroi <= 8000) {
     nivel = "Platina";
 }
-else if (xpHeroi <= 9000){
+else if (xpHeroi <= 9000) {
     nivel = "Ascendente";
 }
-else if (xpHeroi <= 10000){
+else if (xpHeroi <= 10000) {
     nivel = "Imortal";
 }
 else {
     nivel = "Radiante";
 }
 
-console.log("-----------------------------------")
-console.log("O seu Campeão é: ** " + heroiSelecionado +  " ** e " + "Está no nível: ** " + nivel + " **")
+console.log("-----------------------------------------------------------")
+console.log("O seu Campeão é: ** " + heroiSelecionado + " ** e " + "Está no nível: ** " + nivel + " **")
+console.log("-----------------------------------------------------------")
